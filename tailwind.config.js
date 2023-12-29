8d59e7456d4dda2ce08a61966181f9e69706c054
elmehdi elfatihi <elmehdielfatihi1111@gmail.com>
2023-12-29 15:28:18 +0100

    update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,22 +6,24 @@ export default {
   ],
   theme: {
     extend: {
-      purple: '#A729F5',
-      'dark-navy': '#313E51',
-      'navy': '#3B4D66',
-      'grey-navy': '#626C7F',
-      'light-bluish': '#ABC1E1',
-      'light-grey': '#F4F6FA',
-      'green': '#26D782',
-      'red': '#EE5454'
+      colors: {
+        purple: '#A729F5',
+        'dark-navy': '#313E51',
+        'navy': '#3B4D66',
+        'grey-navy': '#626C7F',
+        'light-bluish': '#ABC1E1',
+        'light-grey': '#F4F6FA',
+        'green': '#26D782',
+        'red': '#EE5454'
+      },
+      fontSize: {
+        display: '9rem',
+        'headingL': '4rem',
+        'headingM': '2.25rem',
+        'headingS': '1.75rem',
+        'bodyM': '0.75rem',
+      }
     },
-    fontSize: {
-      display: '9rem',
-      'headingL': '4rem',
-      'headingM': '2.25rem',
-      'headingS': '1.75rem',
-      'bodyM': '0.75rem',
-    }
   },
   plugins: [],
 }
